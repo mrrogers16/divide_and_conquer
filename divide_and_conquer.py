d7d8085537ef23d631d065257a4e335b130e0461
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2024-09-30 16:19:32 -0500

    Think im almost done. Just need command line arguments

--- divide_and_conquer.py
+++ divide_and_conquer.py
@@ -29,10 +29,84 @@ def read_stocks(filename):
         exit(1)
 
 
+def find_max_profit(prices):
+    n = len(prices)
+    if n < 2:
+        return 0  # No profit can be made with less than 2 prices
+
+    # Initialize differences array
+    diff = []
+    # Compute the differences between consecutive prices
+    for i in range(n - 1):
+        current_price = prices[i]
+        next_price = prices[i + 1]
+        difference = next_price - current_price
+        diff.append(difference)
+
+    max_profit = max_subarray(diff, 0, n - 2)
+
+    if max_profit < 0:
+        return 0
+    else:
+        return max_profit
+
+
+def max_subarray(diff, low, high):
+    # Base case: one element
+    if low == high:
+        return diff[low]
+
+    # Find mid point
+    mid = (low + high) // 2
+    # Find max subarray sum in left half
+    left_max = max_subarray(diff, low, mid)
+    # Find max subarray sum in right half
+    right_max = max_subarray(diff, mid + 1, high)
+    # Find max subarray sum that crosses midpoint
+    cross_max = max_crossing_subarray(diff, low, mid, high)
+
+    # Return max of the three
+    if left_max >= right_max and left_max >= cross_max:
+        return left_max
+    elif right_max >= left_max and right_max >= cross_max:
+        return right_max
+    else:
+        return cross_max
+
+
+def max_crossing_subarray(diff, low, mid, high):
+    current_sum = 0
+    left_sum = diff[mid]
+
+    # Calculate max sum in left starting at mid point and moving left
+    for i in range(mid, low - 1, -1):
+        current_sum += diff[i]
+        if current_sum > left_sum:
+            left_sum = current_sum
+
+    # Calculate max sum in right starting at mid + 1 and moving right
+    # Reset sum
+    current_sum = 0
+    # Handle possible out of bounds access for mid + 1
+    if mid + 1 <= high:
+        right_sum = diff[mid + 1]
+    else:
+        right_sum = 0  # Right has no elements
+
+    for i in range(mid + 1, high + 1, 1):
+        current_sum += diff[i]
+        if current_sum > right_sum:
+            right_sum = current_sum
+    # Combine two sums
+    cross_sum = right_sum + left_sum
+
+    return cross_sum
+
+
 def main():
     # Take in file name
     # TODO Set up command line arguments to take in file name from user
-    file_name = "small_input.txt"
+    file_name = "nasdaq.txt"
 
     # Read in stock prices from file and create array of floats
     prices = read_stocks(filename=file_name)
@@ -42,6 +116,8 @@ def main():
         return
     #
     optimal_profit = find_max_profit(prices)
+
+    print(f"The optimal solution for {file_name} is {optimal_profit}.")
 
 
 if __name__ == "__main__":
